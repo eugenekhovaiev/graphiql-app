4fe948b117a239b871758f73d221fb588174e5c4
XeniyaMV <mehnuchka.xd@gmail.com>
2023-12-15 10:29:00 -0800

    GQL-2 refactor: move About to Main

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,17 @@
-import styles from './main.module.scss';
+import StartScreen from '@/components/StartScreen';
+import AboutProject from '@/components/AboutProject';
+import AboutCourse from '@/components/AboutCourse';
+import AboutUs from '@/components/AboutUs';
 
 function Main(): JSX.Element {
-  return <div className={styles.main}>Main Page</div>;
+  return (
+    <main>
+      <StartScreen />
+      <AboutProject />
+      <AboutUs />
+      <AboutCourse />
+    </main>
+  );
 }
 
 export default Main;
