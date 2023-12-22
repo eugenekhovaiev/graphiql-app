80c354d14c69dfb14b179fb93a2912df74c12f2f
Maria Bogdanova <masha.bogdanoff@gmail.com>
2023-12-22 12:04:29 +0100

    GQL-65 fix: user friendly validation messages

--- src/utils/authFormValidationSchema.ts
+++ src/utils/authFormValidationSchema.ts
@@ -3,27 +3,27 @@ import * as yup from 'yup';
 const schema = yup.object().shape({
   email: yup
     .string()
-    .required('Email is a required field')
+    .required('Please provide your email')
     .matches(
       /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
-      'Enter a valid email address in the format "username@example.com", with a valid top-level domain (e.g., ".com", ".org").'
+      'Please enter a valid email address in the format "username@example.com"'
     ),
 
   password: yup
     .string()
-    .required('Password is a required field')
-    .min(8, 'Password must be at least 8 characters')
-    .matches(/^(?=.*\p{L})/u, 'Password must include at least one letter')
-    .matches(/^(?=.*\p{N})/u, 'Password must include at least one digit')
+    .required('Please set a password for access')
+    .min(8, 'Password should be at least 8 characters long')
+    .matches(/^(?=.*\p{L})/u, 'Password should include at least one letter')
+    .matches(/^(?=.*\p{N})/u, 'Password should include at least one digit')
     .matches(
       /^(?=.*[@$!%*#?&])/u,
-      'Password must include at least one special character'
+      'Password should include at least one special character'
     ),
 
   confirmPassword: yup.string().when({
     is: (exists: string | undefined) => !!exists,
     then: (rule) =>
-      rule.test('match', 'Passwords must match', function (value) {
+      rule.test('match', 'Make sure your passwords match', function (value) {
         return this.parent.password === value;
       }),
   }),
