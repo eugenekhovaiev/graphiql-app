1d464b19e2952c292d199b47c9dc36b8663642a6
Maria Bogdanova <masha.bogdanoff@gmail.com>
2023-12-25 18:49:16 +0100

    GQL-128 feat: add change endpoint button

--- src/components/editorPageComponents/EndpointInput/EndpointInput.tsx
+++ src/components/editorPageComponents/EndpointInput/EndpointInput.tsx
@@ -1,7 +1,13 @@
 import styles from './endpointInput.module.scss';
+import Button from '@/components/ui/Button';
 
 function EndpointInput(): JSX.Element {
-  return <input className={styles.endpointInput} />;
+  return (
+    <div className={styles.endpoint}>
+      <input className={styles.endpoint__input} />
+      <Button title="Change" type="button" styleType="light" />
+    </div>
+  );
 }
 
 export default EndpointInput;
