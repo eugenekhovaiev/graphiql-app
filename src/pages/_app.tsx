7d6a833764d62fae9a0e863ff71f424f98b029bd
Maria Bogdanova <masha.bogdanoff@gmail.com>
2024-01-05 12:35:16 +0100

    GQL-96 feat: add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,7 +19,6 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
       `}</style>
       <Head>
         <meta charSet="UTF-8" />
-        <link rel="icon" href="favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>GraphiQL App</title>
         <meta name="description" content="Our app description" />
